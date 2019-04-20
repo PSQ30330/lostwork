c6d61b9832e54ac531ef12be731452ffae9a1f93
psq <434560987@qq.com>
2019-04-20 14:54:03 +0800

    完成查看选题

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -6,5 +6,7 @@ urlpatterns =[
     url(r'^add_projet/$',views.add_project,name="添加实训题目"),
     url(r'^info/$',views.info,name="学生选题"),
     url(r'^select/$',views.select_project,name="houduan"),
-    url(r'^xuanti/$',views.student_xunti,name ="选题")
+    url(r'^xuanti/$',views.student_xunti,name ="选题"),
+    url(r'^chakan/$',views.chakan,name="查看选题"),
+    url(r'^stu_chakan/$',views.stu_chakan,name="返回题目"),
 ]
--- apps/project/views.py
+++ apps/project/views.py
@@ -10,7 +10,6 @@ from project.models import StudentSelect, TeacherProject
 
 
 def add_project(request):
-
     if request.session.get('is_login', None):
         if request.method == "POST":
             add_form = forms.ProForm(request.POST)
@@ -28,7 +27,7 @@ def add_project(request):
                 if same_pro_title:
                     message = "该题目已经录入，无需重复录入"
 
-                    return render(request,'project/add_projet.html',locals())
+                    return render(request, 'project/add_projet.html', locals())
                 else:
                     new_pro = models.TeacherProject()
                     new_pro.pro_title = pro_title
@@ -41,32 +40,30 @@ def add_project(request):
 
                     return redirect('/teacher/index/')
 
-
     add_form = forms.ProForm()
 
-    return render(request,'project/add_projet.html',locals())
+    return render(request, 'project/add_projet.html', locals())
 
 
 def info(requset):
-    return render(requset,'project/student_select.html')
+    return render(requset, 'project/student_select.html')
+
 
 def select_project(request):
-
     users_list = []
-    req =TeacherProject.objects.all()
-
+    req = TeacherProject.objects.all()
 
     for item in req:
-        user_info= {
-        "pro_id":item.pro_id,
-        "pro_title":item.pro_title,
-        "pro_type":item.pro_type,
-        "pro_teacher":item.pro_teacher,
-        "pro_content":item.pro_content,
-        "pro_count":item.pro_count,
-        "bei_zhu":item.bei_zhu,
-        "fa_za":item.fa_za,
-        "fei_gong":item.fei_gong,
+        user_info = {
+            "pro_id": item.pro_id,
+            "pro_title": item.pro_title,
+            "pro_type": item.pro_type,
+            "pro_teacher": item.pro_teacher,
+            "pro_content": item.pro_content,
+            "pro_count": item.pro_count,
+            "bei_zhu": item.bei_zhu,
+            "fa_za": item.fa_za,
+            "fei_gong": item.fei_gong,
 
         }
         users_list.append(user_info)
@@ -84,22 +81,22 @@ def student_xunti(requset):
             pro_id = stu_xunti_form.cleaned_data['stu_proid']
             id = models.StudentSelect.objects.filter().count()
             if id > 0:
-                message = "已经选过实训题目，无需再添加"
-                return render(requset,'project/student_xuanti.html',locals())
+                message = "已经选过实训; 题目，无需再添加"
+                return render(requset, 'project/student_xuanti.html', locals())
             else:
-                neirong = models.TeacherProject.objects.filter(pro_id= pro_id).all().first()
+                neirong = models.TeacherProject.objects.filter(pro_id=pro_id).all().first()
                 stu_username = username
-                pro_title =neirong.pro_title
-                pro_type =neirong.pro_type
-                pro_teacher =neirong.pro_teacher
+                pro_title = neirong.pro_title
+                pro_type = neirong.pro_type
+                pro_teacher = neirong.pro_teacher
                 pro_content = neirong.pro_content
-                pro_count =neirong.pro_count
-                fei_gong =neirong.fei_gong
+                pro_count = neirong.pro_count
+                fei_gong = neirong.fei_gong
 
-                new_user =models.StudentSelect()
-                new_user.stu_username=stu_username
+                new_user = models.StudentSelect()
+                new_user.stu_username = stu_username
                 new_user.stu_proid = pro_id
-                new_user.pro_title =pro_title
+                new_user.pro_title = pro_title
                 new_user.pro_type = pro_type
                 new_user.pro_teacher = pro_teacher
                 new_user.pro_content = pro_content
@@ -108,9 +105,36 @@ def student_xunti(requset):
 
                 new_user.save()
 
-
                 return redirect('/student/index/')
-
 
     stu_xunti_form = forms.XuanTiForm
     return render(requset, 'project/student_xuanti.html', locals())
+
+
+def chakan(request):
+    return render(request, 'project/chakan.html')
+
+
+def stu_chakan(requset):
+    users_list = []
+    username = requset.session.get('username')
+
+    req = models.StudentSelect.objects.filter(stu_username=username).all()
+
+    for item in req:
+        user_info = {
+            "stu_username": item.stu_username,
+            "stu_proid": item.stu_proid,
+            "pro_title": item.pro_title,
+            "pro_type": item.pro_type,
+            "pro_teacher": item.pro_teacher,
+            "pro_content": item.pro_content,
+            "pro_count": item.pro_count,
+            "fei_gong": item.fei_gong,
+
+        }
+        users_list.append(user_info)
+    user_dic = {}
+
+    user_dic['data'] = users_list
+    return HttpResponse(json.dumps(user_dic))
